8c91a8119db3b190644aa198e32b3f38b8ea5370
Yogesh12P <patil1yogesh21@gmail.com>
2017-03-17 04:58:48 -0700

    program for calculator

--- Calculatorp/Calculatorp/ViewController.h
+++ Calculatorp/Calculatorp/ViewController.h
@@ -13,7 +13,20 @@
 @property (strong, nonatomic) IBOutlet UITextField *text1;
 - (IBAction)add:(id)sender;
 - (IBAction)sub:(id)sender;
+- (IBAction)mul:(id)sender;
+- (IBAction)div:(id)sender;
 - (IBAction)equal:(id)sender;
+- (IBAction)bt1:(id)sender;
+- (IBAction)bt2:(id)sender;
+- (IBAction)bt3:(id)sender;
+- (IBAction)bt4:(id)sender;
+- (IBAction)bt5:(id)sender;
+- (IBAction)bt6:(id)sender;
+- (IBAction)bt7:(id)sender;
+- (IBAction)bt8:(id)sender;
+- (IBAction)bt9:(id)sender;
+- (IBAction)bt0:(id)sender;
+- (IBAction)clear:(id)sender;
 @property (strong, nonatomic) IBOutlet UILabel *label1;
 
 @end
--- Calculatorp/Calculatorp/ViewController.m
+++ Calculatorp/Calculatorp/ViewController.m
@@ -25,7 +25,7 @@
     // Dispose of any resources that can be recreated.
 }
 int a,b;
-int c=0;
+float c=0;
 
 - (IBAction)add:(id)sender {
     a = [_text1.text intValue];
@@ -37,14 +37,91 @@ int c=0;
 
 - (IBAction)sub:(id)sender {
     a = [_text1.text intValue];
+    if(c>a)
+    c=c-a;
+    else
     c=a-c;
+    
     _text1.text = @"";
+    
     //[self.text1 setText:[NSString stringWithFormat:@"%d",c]];
 }
 
-- (IBAction)equal:(id)sender {
-    a= [_text1.text intValue];
-    c=c+a;
-    [self.label1 setText:[NSString stringWithFormat:@"%d",c]];
+- (IBAction)mul:(id)sender {
+    a = [_text1.text intValue];
+    if(c==0)
+        c=a;
+    else
+        c=c*a;
+    
+    _text1.text = @"";
+
 }
+
+- (IBAction)div:(id)sender {
+    a= [_text1.text intValue];
+    if(c==0)
+        c=a;
+    else
+        c=c/a;
+
+    _text1.text = @"";
+    
+    
+}
+
+- (IBAction)equal:(id)sender {
+    //a= [_text1.text intValue];
+    //c=c+a;
+    [self.label1 setText:[NSString stringWithFormat:@"%f",c]];
+}
+
+- (IBAction)bt1:(id)sender {
+    _text1.text = [NSString stringWithFormat:@"%@1",_text1.text];
+    
+}
+
+- (IBAction)bt2:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@2",_text1.text];
+}
+
+- (IBAction)bt3:(id)sender {
+    _text1.text = [NSString stringWithFormat:@"%@3",_text1.text];
+}
+
+- (IBAction)bt4:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@4",_text1.text];
+}
+
+- (IBAction)bt5:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@5",_text1.text];
+}
+
+- (IBAction)bt6:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@6",_text1.text];
+}
+
+- (IBAction)bt7:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@7",_text1.text];
+}
+
+- (IBAction)bt8:(id)sender {
+     _text1.text = [NSString stringWithFormat:@"%@8",_text1.text];
+}
+
+- (IBAction)bt9:(id)sender {
+ _text1.text = [NSString stringWithFormat:@"%@9",_text1.text];
+}
+
+- (IBAction)bt0:(id)sender {
+ _text1.text = [NSString stringWithFormat:@"%@0",_text1.text];
+}
+
+- (IBAction)clear:(id)sender {
+    _text1.text = @"";
+    c=0;
+    [self.label1 setText:[NSString stringWithFormat:@""]];
+}
+
+
 @end
